59e6115d35e0c4cd086d70477abecc62651c8550
NaturalBornCamper <marco@naturalborncamper.com>
2018-05-30 00:31:59 -0400

    =Removes lots of DOM generation with JS to speed up refreshes

--- all.js
+++ all.js
@@ -153,7 +153,6 @@ function all() {
                 b0 = '1m';
             }
             $("#graph-info-" + k + "-" + i).html((dcmp((k + i) * 3, 3, gsbms) / ss).toFixed(dd) + sbms1[9])
-            // htm('g' + (k + i), '<V5> ' + b0 + '<v' + (k) + '>' + n + '</v' + (k) + '><v' + (k + 1) + '>' + m + '</v' + (k + 1) + '>' + sp + (dcmp((k + i) * 3, 3, gsbms) / ss).toFixed(dd) + sbms1[9] + '</V5>');
         }
     }
 
@@ -169,7 +168,7 @@ function all() {
         var bv = pv3 = sv = max1 = min1 = 0;
         for (x1 = 0; x1 < 8; x1++) {
             var cv = dcmp((x1 * 2) + 8, 2, sbms) / 1000;
-            var selector = "span:eq("+x1+")";
+            var selector = "span:eq(" + x1 + ")";
             if (sbms2[9] == x1 + 1) {
                 $("#d2, #d3").find(selector).removeClass().addClass('mn1');
             }
@@ -179,19 +178,21 @@ function all() {
             }
 
             $("#d3").find(selector).html(cv.toFixed(3));
-            $("#d4").find(selector).html(sbms2[x1] != 1? "V": "<");
+            $("#d4").find(selector).html(sbms2[x1] != 1 ? "V" : "<");
 
             bv += cv;
             if (cv > 0) {
                 var min = dcmp(5, 2, xsbms) / 1000;
                 var max = dcmp(3, 2, xsbms) / 1000;
-                cv = (cv-min)/(max-min)*100;
+                cv = (cv - min) / (max - min) * 100;
             }
 
-            $("#cell"+x1).css('width', cv + '%');
-            $("#cell"+x1).css('background', getHslColor(cv));
+            $("#cell" + x1).css('width', cv + '%');
+            $("#cell" + x1).css('background', getHslColor(cv));
         }
 
+
+        var domCounter = 0;
         for (x1 = 0; x1 < 7; x1++) {
             var n2 = w[8] = w[9] = w[10] = w[11] = '';
             var cv = dcmp((x1 * 3) + 29, 3, sbms) / 1000;
@@ -199,16 +200,12 @@ function all() {
             var enA = dcmp(x1 * 6, 6, eA);
             if (x1 == 0) {
                 n2 = sbms.charAt(28);
-                w[8] = '[A]' + r;
-                w[9] = '[W]' + r;
-                w[10] = '[mAh]' + r;
-                w[11] = '[Wh]' + r;
             }
-            ;
+
             if (x1 == 1 || x1 == 2) {
                 pv3 += cv;
             }
-            ;
+
             if (x1 == 3) {
                 sv = cv;
             }
@@ -221,34 +218,25 @@ function all() {
             if (x1 == 6) {
                 cv = sv;
             }
+
             if (x1 != 3) {
-                w[3] += sbms1[x1 + 1] + r;
-                w[4] += w[8] + n2 + cv.toFixed(3) + r;
-                w[5] += w[9] + n2 + (cv * bv).toFixed(1) + r;
-                w[6] += w[10] + fN(enA) + r;
-                w[7] += w[11] + fN((enW / 10).toFixed(1)) + r;
+                var selector = "span:eq(" + domCounter + ")";
+                $("#d7").find(selector).html(n2 + cv.toFixed(3));
+                $("#d8").find(selector).html(n2 + (cv * bv).toFixed(1));
+                $("#d9").find(selector).html(fN(enA));
+                $("#d10").find(selector).html(fN((enW / 10).toFixed(1)));
+
+                ++domCounter;
             }
         }
 
-        for (i = 6; i < 11; i++) {
-            htm('d' + i, w[i - 3]);
-        }
-
-        $('#infoBarTitle1').html('System type');
         $('#infoBarValue1').html(dcmp(7, 1, xsbms));
-        $('#infoBarTitle2').html('Maximum capacity');
         $('#infoBarValue2').html(dcmp(8, 3, xsbms) + sbms1[8]);
-        $('#infoBarTitle3').html('System status');
         $('#infoBarValue3').html(dcmp(56, 3, sbms) + r);
-        $('#infoBarTitle4').html('Interior temperature');
         $('#infoBarValue4').html(((dcmp(24, 2, sbms) / 10) - 45).toFixed(1) + '&#8451');
-        $('#infoBarTitle5').html('Exterior temperature');
         $('#infoBarValue5').html(((dcmp(26, 2, sbms) / 10) - 45).toFixed(1) + '&#8451');
-        $('#infoBarTitle6').html('Battery voltage');
         $('#infoBarValue6').html(bv.toFixed(3) + 'V');
-        $('#infoBarTitle7').html('Cell &#916');
         $('#infoBarValue7').html(((max1 - min1) * 1000).toFixed(0) + 'mV');
-        // htm('d' + 12, 'Type: ' + dcmp(7, 1, xsbms) + ' Cap: ' + dcmp(8, 3, xsbms) + sbms1[8] + ' Status: ' + dcmp(56, 3, sbms) + r + 'SBMS Temp Int: ' + ((dcmp(24, 2, sbms) / 10) - 45).toFixed(1) + '&#8451 Ext: ' + ((dcmp(26, 2, sbms) / 10) - 45).toFixed(1) + '&#8451' + r + 'BattVoltage <Val>' + bv.toFixed(3) + '</Val> V Cell &#916 <Val>' + ((max1 - min1) * 1000).toFixed(0) + '</Val> mV');
     }
 
 }
